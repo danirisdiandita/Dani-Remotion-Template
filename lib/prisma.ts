c0ed07aa01cadf9197c5f6f164aaeb4b1a72d3fd
dani <normadani2016@gmail.com>
2026-04-05 18:03:08 +0700

    chore: Production suite build & bundle success - 2026-04-05 18:03

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,20 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { ENV } from "@/config/constant";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaNeon } from '@prisma/adapter-neon';
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connString = ENV.db.direct;
+let adapter = null;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+// Determine adapter based on connection string or explicit env (using simple logic for now)
+if (connString.includes("neon.tech")) {
+  adapter = new PrismaNeon({ connectionString: connString });
+} else {
+  adapter = new PrismaPg({ connectionString: connString });
+}
 
-export { prisma };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
